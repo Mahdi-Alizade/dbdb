488de06913037f36f66a1f8e095e27f1e6ee055a
Glitch20057 <ma6962191@gmail.com>
2026-09-17 11:15:36 +0330

    feat: add dictionary iteration methods and safe disk compaction

--- dbdb/interface.py
+++ dbdb/interface.py
@@ -1,3 +1,4 @@
+import os
 from dbdb.binary_tree import BinaryTree
 from dbdb.physical import Storage
 
@@ -39,6 +40,53 @@ class DBDB:
 
     def __len__(self):
         return len(self._tree)
+
+    def __iter__(self):
+        self._assert_not_closed()
+        for key, _ in self._tree.items():
+            yield key
+
+    def keys(self):
+        return list(self)
+
+    def values(self):
+        self._assert_not_closed()
+        return [value for _, value in self._tree.items()]
+
+    def items(self):
+        self._assert_not_closed()
+        return list(self._tree.items())
+
+    def compact(self):
+        """
+        Cleans up disk space by copying only the live, committed nodes to a new file,
+        replacing the old bloated file entirely.
+        """
+        self._assert_not_closed()
+        
+        current_path = self._storage._f.name
+        compact_path = current_path + ".compact"
+
+        # 1. Create a fresh temporary database
+        new_db = connect(compact_path)
+
+        # 2. Iterate through live items and write to the new DB
+        for key, value in self._tree.items():
+            new_db[key] = value
+        
+        new_db.commit()
+        new_db.close()
+
+        # 3. Safely close current storage to release OS file locks (Crucial for Windows)
+        self.close()
+
+        # 4. Atomically swap files (replace the bloated file with the fresh one)
+        os.replace(compact_path, current_path)
+
+        # 5. Reconnect current instance to the newly compacted file
+        f = open(current_path, "r+b")
+        self._storage = Storage(f)
+        self._tree = BinaryTree(self._storage)
 
 
 def connect(dbname):
